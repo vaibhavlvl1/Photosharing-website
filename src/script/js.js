6fd06268d262613de0f74d7b9b0eb6a1b60bea7a
vaibhavlvl1 <acevaibhav14@gmail.com>
2023-12-04 10:25:42 +0530

    final

---
+++ src/script/js.js
@@ -0,0 +1,7 @@
+// navbar active state toggle
+
+// $(function () {
+//     $(".nav-link").on("click", function () {
+//         $(this).addClass("active").siblings().removeClass('active');
+//     })
+// })
